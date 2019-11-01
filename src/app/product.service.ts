d2955a92952ce65f7da909f86a0273480602afd3
Samtimberlan <timudoma@gmail.com>
2019-11-01 17:15:38 +0100

    describe data using interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
+import { Observable } from "rxjs/Observable";
+import { Album } from "./album";
 
 @Injectable()
 export class ProductService {
@@ -8,7 +10,7 @@ export class ProductService {
     console.log("Data service connected...");
   }
   private _albumUrl: string = "../assets/album.json";
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => response:<Album>.json());
   }
 }
