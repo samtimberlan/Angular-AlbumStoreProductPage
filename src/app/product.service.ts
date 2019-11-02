2f8710bd560069d825a1b4b7f9396dd8c24214f1
Samtimberlan <timudoma@gmail.com>
2019-11-02 03:35:19 +0100

    add product listing

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
 import { Observable } from "rxjs/Observable";
 import { Album } from "./album";
+import { Product } from "./product";
 
 @Injectable()
 export class ProductService {
@@ -10,9 +11,16 @@ export class ProductService {
     console.log("Data service connected...");
   }
   private _albumUrl: string = "../assets/album.json";
+  private _productsUrl: string = "../assets/products.json";
   getAlbum(id: number): Observable<Album> {
     return this._http
       .get(this._albumUrl)
       .map(response => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http
+      .get(this._productsUrl)
+      .map(response => <Product[]>response.json());
   }
 }
